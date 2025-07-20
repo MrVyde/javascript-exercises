7abded175c41e77c51347727da9fd8e122e4be24
MrVyde <Mrvyde@outlook.com>
2025-07-20 17:40:44 +0100

    Solve repeat String exercise and pass test

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,25 @@
-const repeatString = function() {
-
+const repeatsString = function(string, hey) {
+ return string.repeat(hey);
 };
+const repeattext = function(text, hello) {
+    return text.repeat(hello);
+};
+const repeattxt = function(txt, hi) {
+    return txt.repeat(hi);
+};
+const repeatword = function(word, bye) {
+    return word.repeat(bye);
+};
+
+const repeatString = function (word, times) {
+  if (times < 0) return "ERROR";
+  let string = "";
+  for (let i = 0; i < times; i++) {
+    string += word;
+  }
+  return string;
+};
+
 
 // Do not edit below this line
 module.exports = repeatString;
