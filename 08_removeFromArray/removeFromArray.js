a60d113331bd3aaa8475f311e292294182c60b7d
MrVyde <Mrvyde@outlook.com>
2025-07-20 19:50:10 +0100

    Solve removeFromArray.js exercise and pass test

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArraY = function(array, value) {
+    return array.filter(item => item !== value);
+};
+const removeFromArray = function(array, ...valuesToRemove) {
+    return array.filter(item => !valuesToRemove.includes(item));
+};
+const removefromArray = function removeAll(array, ...valuesToRemove) {
+    return array.filter(item => item !== valuesToRemove);
 };
 
 // Do not edit below this line
