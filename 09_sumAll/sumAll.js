27c64bc3e3183fcdca22194a0398ab888db6622c
MrVyde <Mrvyde@outlook.com>
2025-07-20 20:08:57 +0100

    Solve sumAll.js exercise and pass test

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+if (
+    !Number.isInteger(start) ||
+    !Number.isInteger(end) ||
+    start < 0 ||
+    end < 0
+  ) {
+    return "ERROR";
+  }
 
+    const min = Math.min(start, end);
+  const max = Math.max(start, end);
+
+  let total = 0;
+  for (let i = min; i <= max; i++) {
+    total += i;
+  }
+  return total;
 };
 
 // Do not edit below this line
