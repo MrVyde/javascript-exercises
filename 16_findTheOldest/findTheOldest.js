321580e635947b0deba2c971d2d631d800eb3aeb
MrVyde <Mrvyde@outlook.com>
2025-07-21 11:54:51 +0100

    Solve findTheOldest.js exercise and pass test

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,23 @@
-const findTheOldest = function() {
+const FindTheOldest = function(people) {
+    return people.reduce((oldest, current) => {
+    const oldestAge = oldest.yearOfDeath - oldest.yearOfBirth;
+    const currentAge = current.yearOfDeath - current.yearOfBirth;
+    return currentAge > oldestAge ? current : oldest;
+  });
 
 };
 
+const findTheOldest = function(people) {
+  return people.reduce((oldest, current) => {
+    const currentYear = new Date().getFullYear();
+
+    const getAge = person => {
+      const deathYear = person.yearOfDeath || currentYear;
+      return deathYear - person.yearOfBirth;
+    };
+
+    return getAge(current) > getAge(oldest) ? current : oldest;
+  });
+};
 // Do not edit below this line
 module.exports = findTheOldest;
